15c666caa82b3510563ebf135e568c8d6ddde79d
Ameeruddin Noorbasha <ameeruddinnoorbasha786@gmail.com>
2025-11-19 14:24:38 +0530

    second commit

--- app/src/main/scala/mvc/repositories/ArtistRepositoryForDelete.scala
+++ app/src/main/scala/mvc/repositories/ArtistRepositoryForDelete.scala
@@ -6,8 +6,10 @@ import scala.util.Using
 class ArtistRepositoryForDelete(configLoader: ConfigLoader = new ConfigLoader()) {
 
   def deleteFromSql(tableName: String): Int = {
-    val deleteQuery = s"delete from ${tableName} where user_id = 'user1'"
-//    val deleteQuery = s"delete from ${tableName} where user_id = '2'"
+    //For H2 DB
+    val deleteQuery = s"delete from ${tableName.toUpperCase} where user_id = 'user1'"
+
+//    val deleteQuery = s"delete from ${tableName.toUpperCase} where user_id = '2'"
     Using.resource(configLoader.getConnection) { connection =>
       Using.resource(connection.prepareStatement(deleteQuery)) { preparedStatement =>
         preparedStatement.executeUpdate()
