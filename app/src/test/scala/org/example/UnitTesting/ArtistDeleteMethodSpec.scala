80ce1fa22849b89b4ab73518d65cc2578bfda90e
Ameeruddin Noorbasha <ameeruddinnoorbasha786@gmail.com>
2025-11-20 10:03:25 +0530

    Third commit

--- app/src/test/scala/org/example/UnitTesting/ArtistDeleteMethodSpec.scala
+++ app/src/test/scala/org/example/UnitTesting/ArtistDeleteMethodSpec.scala
@@ -6,6 +6,8 @@ import org.scalatest.flatspec.AnyFlatSpec
 import org.scalatest.matchers.should.Matchers
 import org.scalatestplus.mockito.MockitoSugar
 
+import java.sql.SQLException
+
 class ArtistDeleteMethodSpec extends AnyFlatSpec with Matchers with MockitoSugar with TestMockUtils {
   "ArtistRepositoryForDelete.deleteFromSql" should "return the number of deleted rows when deletion is successful" in {
     val (mockConfigLoader, mockConnection, mockPreparedStatement) = createDatabaseMocks()
@@ -43,37 +45,19 @@ class ArtistDeleteMethodSpec extends AnyFlatSpec with Matchers with MockitoSugar
     verify(mockConnection).close()
   }
 
-  it should "use the correct table name in the SQL query" in {
-    val (mockConfigLoader, mockConnection, mockPreparedStatement) = createDatabaseMocks()
+  it should "throw exception when preparing statement fails" in {
+    val (mockConfigLoader, mockConnection, _) = createDatabaseMocks()
     val repository = new ArtistRepositoryForDelete(mockConfigLoader)
 
     when(mockConfigLoader.getConnection).thenReturn(mockConnection)
-    when(mockConnection.prepareStatement("delete from CUSTOM_TABLE where user_id = '2'"))
-      .thenReturn(mockPreparedStatement)
-    when(mockPreparedStatement.executeUpdate()).thenReturn(1)
+    when(mockConnection.prepareStatement("delete from ARTISTS where user_id = '2'"))
+      .thenThrow(new SQLException("Invalid SQL syntax"))
 
-    val result = repository.deleteFromSql("custom_table")
-
-    result shouldBe 1
-    verify(mockConnection).prepareStatement("delete from CUSTOM_TABLE where user_id = '2'")
-  }
-
-  it should "handle different table names correctly" in {
-    val testTables = List("users", "artists", "albums", "tracks")
-
-    testTables.foreach { tableName =>
-      val (mockConfigLoader, mockConnection, mockPreparedStatement) = createDatabaseMocks()
-      val repository = new ArtistRepositoryForDelete(mockConfigLoader)
-
-      when(mockConfigLoader.getConnection).thenReturn(mockConnection)
-      when(mockConnection.prepareStatement(s"delete from ${tableName.toUpperCase} where user_id = '2'"))
-        .thenReturn(mockPreparedStatement)
-      when(mockPreparedStatement.executeUpdate()).thenReturn(1)
-
-      val result = repository.deleteFromSql(tableName)
-
-      result shouldBe 1
-      verify(mockConnection).prepareStatement(s"delete from ${tableName.toUpperCase} where user_id = '2'")
+    val exception = intercept[SQLException] {
+      repository.deleteFromSql("artists")
     }
+
+    exception.getMessage shouldBe "Invalid SQL syntax"
+    verify(mockConnection).close() // Connection should still be closed
   }
 }
